b4afd52f81da12c97291105b017ab5f6c14115e8
Rikuto428 <43780220+Rikuto428@users.noreply.github.com>
2019-02-25 01:55:17 +0900

    Add PlayerController.cs

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	void Start () {
+
+    }
+	
+	void Update () {
+
+        //移動
+        if (Input.GetKey(KeyCode.LeftArrow)) transform.Translate(-0.1f, 0, 0);
+        if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(0.1f, 0, 0);
+        if (Input.GetKey(KeyCode.UpArrow)) transform.Translate(0, 0.1f, 0);
+        if (Input.GetKey(KeyCode.DownArrow)) transform.Translate(0, -0.1f, 0);
+
+    }
+}
