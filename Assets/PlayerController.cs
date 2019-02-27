f44ce359fdd20baf43ce2e3ec382af1f287fd7ee
Rikuto428 <43780220+Rikuto428@users.noreply.github.com>
2019-02-28 01:26:12 +0900

    敵キャラの移動
    Edit PlayerController.cs
    Add Move_StandEnemy.cs

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	void Start () {
+    Rigidbody2D rigid2D;
+    public float speed = 5;
+
+    void Start () {
+
+        this.rigid2D = GetComponent<Rigidbody2D>();
 
     }
 	
 	void Update () {
 
-        //移動
-        if (Input.GetKey(KeyCode.LeftArrow)) transform.Translate(-0.1f, 0, 0);
-        if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(0.1f, 0, 0);
-        if (Input.GetKey(KeyCode.UpArrow)) transform.Translate(0, 0.1f, 0);
-        if (Input.GetKey(KeyCode.DownArrow)) transform.Translate(0, -0.1f, 0);
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector2 direction = new Vector2(x, y).normalized;
+        this.rigid2D.velocity = direction * this.speed;
 
     }
 }
